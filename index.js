a85944875da6b22571842bf464b133fda636b510
Danilo <danilomartins75@gmail.com>
2025-07-04 21:35:34 -0300

    feat: configure .env files

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require('./src/routes')
 const app = express();
 app.use(express.json());
-const port = 3333;
+
+
+const PORT= Number(process.env.PORT )|| 3333;
+console.log({PORT,db: process.env. DATABASE_URL})
 
 async function connectDatabase(){
-  await mongoose.connect("mongodb://localhost:27017/test");
+  await mongoose.connect(process.env.DATABASE_URL);
 }
 
 
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   connectDatabase().catch((error) => {
     console.log(`Error Connecting database: ${error}`);
   });
